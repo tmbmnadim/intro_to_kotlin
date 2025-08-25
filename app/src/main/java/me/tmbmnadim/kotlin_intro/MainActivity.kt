86dc84ab32ddee32f0d1a66f7b395f5a8845519e
Mansur Nadim <tmbmnadim@gmail.com>
2025-08-25 19:01:19 +0600

    Second Commit
    Rows and Columns

--- app/src/main/java/me/tmbmnadim/kotlin_intro/MainActivity.kt
+++ app/src/main/java/me/tmbmnadim/kotlin_intro/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
@@ -11,10 +14,12 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import me.tmbmnadim.kotlin_intro.ui.theme.IntroTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,8 +29,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             IntroTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    GreetingText(
+                        message = "Hello, there! Are you ready?",
+                        from = " - From Nadim",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -35,19 +41,30 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun GreetingText(message: String,from: String, modifier: Modifier = Modifier) {
     Surface(color = MaterialTheme.colorScheme.background) {
-        Text(
-            text = "Hello $name!",
-            modifier = modifier.padding(24.dp)
-        )
+        Column(modifier=modifier.fillMaxSize(), verticalArrangement = Arrangement.Center) {
+            Text(
+                text = message,
+                modifier = modifier.padding(24.dp),
+                fontSize = 100.sp,
+                lineHeight = 116.sp,
+                textAlign = TextAlign.Center
+
+            )
+            Text(
+                text = from,
+                modifier=modifier.padding(24.dp).align(Alignment.End),
+                fontSize = 36.sp
+            )
+        }
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun GreetingPreview() {
+fun KotlinIntroPreview() {
     IntroTheme {
-        Greeting("Nadim")
+        GreetingText("Hello, there! Are you ready?", " - From Nadim")
     }
 }
