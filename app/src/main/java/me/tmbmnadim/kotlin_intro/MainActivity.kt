71c06abb65585b0e366c4e407d79ad4e58d2a104
Mansur Nadim <tmbmnadim@gmail.com>
2025-08-26 19:09:10 +0600

    Third Commit
    Grid and Ratio

--- app/src/main/java/me/tmbmnadim/kotlin_intro/MainActivity.kt
+++ app/src/main/java/me/tmbmnadim/kotlin_intro/MainActivity.kt
@@ -4,18 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -28,12 +37,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             IntroTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    GreetingText(
-                        message = "Hello, there! Are you ready?",
-                        from = " - From Nadim",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Quadrants()
                 }
             }
         }
@@ -41,23 +49,103 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun GreetingText(message: String,from: String, modifier: Modifier = Modifier) {
-    Surface(color = MaterialTheme.colorScheme.background) {
-        Column(modifier=modifier.fillMaxSize(), verticalArrangement = Arrangement.Center) {
-            Text(
-                text = message,
-                modifier = modifier.padding(24.dp),
-                fontSize = 100.sp,
-                lineHeight = 116.sp,
-                textAlign = TextAlign.Center
-
+fun Quadrants(child: @Composable () -> Unit = {}) {
+    Column(Modifier.fillMaxSize()) {
+        Row(Modifier.weight(1f)) {
+            Box(Modifier
+                .weight(1f)
+                .fillMaxHeight()
+                .border(
+                    1.dp,
+                    Color.Black,
+                )
             )
-            Text(
-                text = from,
-                modifier=modifier.padding(24.dp).align(Alignment.End),
-                fontSize = 36.sp
+            {
+                child()
+            }
+            Box(Modifier
+                .weight(1f)
+                .fillMaxHeight()
+                .border(
+                    1.dp,
+                    Color.Black,
+                )
             )
+            {
+                child()
+            }
         }
+        Row(Modifier.weight(1f)) {
+            Box(Modifier
+                .weight(1f)
+                .fillMaxHeight()
+                .border(
+                    1.dp,
+                    Color.Black,
+                )
+            )
+            {
+                child()
+            }
+            Box(Modifier
+                .weight(1f)
+                .fillMaxHeight()
+                .border(
+                    1.dp,
+                    Color.Black,
+                )
+            )
+            {
+                child()
+            }
+        }
+    }
+}
+
+@Composable
+fun TitleText(modifier: Modifier = Modifier) {
+    Text(
+        text = stringResource(R.string.tutorial_title),
+        modifier = modifier
+            .padding(16.dp),
+        fontSize = 24.sp,
+        lineHeight = 26.sp,
+        textAlign = TextAlign.Start
+
+    )
+}
+
+@Composable
+fun SubtitleText(modifier: Modifier = Modifier) {
+    Text(
+        text = stringResource(R.string.tutorial_subtitle),
+        modifier = modifier
+            .padding(start = 16.dp, end = 16.dp),
+        textAlign = TextAlign.Justify
+
+    )
+}
+
+@Composable
+fun BodyText(modifier: Modifier = Modifier) {
+    Text(
+        text = stringResource(R.string.tutorial_details),
+        modifier = modifier
+            .padding(16.dp),
+        textAlign = TextAlign.Justify
+
+    )
+}
+
+@Composable
+fun TitleImage(modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.bg_compose_background);
+    Box(modifier) {
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.FillWidth,
+        )
     }
 }
 
@@ -65,6 +153,15 @@ fun GreetingText(message: String,from: String, modifier: Modifier = Modifier) {
 @Composable
 fun KotlinIntroPreview() {
     IntroTheme {
-        GreetingText("Hello, there! Are you ready?", " - From Nadim")
+        Column {
+            Quadrants {
+                Column {
+                    TitleImage()
+                    TitleText()
+                    SubtitleText()
+                    BodyText()
+                }
+            }
+        }
     }
 }
